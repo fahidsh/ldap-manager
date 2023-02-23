e3a621587c8e8f592584c18562ebf499aca25129
Fahid Shehzad <git@shehzad.eu>
2023-02-23 17:04:01 +0100

    set the start-time variables

--- ldap-manager.sh
+++ ldap-manager.sh
@@ -52,3 +52,29 @@ function updateConfigValue() {
 }
 
 
+<<COMMENT
+*******************************************
+    Variablen die am Anfang gesetzt werden
+*******************************************
+COMMENT
+# LDAP Domain aus der config Datei lesen
+LDAP_Domain=$(readConfigValue "LDAP_Domain")
+# wenn LDAP Domain nicht leer ist, dann LDAP Prefix setzen
+[ -n "$LDAP_Domain" ] && LDAP_Prefix=$(echo $LDAP_Domain | sed 's/\./,dc=/g' | sed 's/^/dc=/')
+# variable um die nächste UID/GUID zu ermitteln
+# zu letzt verwendete UID/GUID aus der config Datei lesen
+LDAP_uid=$(readConfigValue "LDAP_uid")
+# LDAP admin passwort aus der config Datei lesen
+# es ist der dc=admin,dc=example,dc=com passwort
+LDAP_Admin_Pass=$(readConfigValue "LDAP_Admin_Pass")
+# LDAP config passwort aus der config Datei lesen
+# es ist der cn=admin,cn=config passwort
+LDAP_Config_Pass=$(readConfigValue "LDAP_Config_Pass")
+# MYSQL Root Passwort aus der config Datei lesen
+MYSQL_Root_Pass=$(readConfigValue "MYSQL_Root_Pass")
+# variable um den status der apt update zu speichern
+# bei jede start der script (Skript-Session) wird es auf false gesetzt
+# nach ausführen von apt update wird es auf true gesetzt
+isAptUpdate=false
+# Hostname der Maschine (VM/Server)
+Hostname=$(hostname -f)
